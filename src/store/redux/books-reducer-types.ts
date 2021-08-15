c69b2b2a213797757031d48557ca3d0068470cdb
Dmitriy <fomenkodmitriy1990@gmail.com>
2021-08-15 18:30:29 +0300

    add reducer actions

--- src/store/redux/books-reducer-types.ts
+++ src/store/redux/books-reducer-types.ts
@@ -18,14 +18,54 @@ export interface BookType {
     description: string;
     categories: string[];
 }
+type category = "all" | "art" | "biography" | "computers" | "history" | "medical" | "poetry";
+type sort = "relevance" | "newest";
 export interface BookStateType {
     filter: string;
-    category: "all" | "art" | "biography" | "computers" | "history" | "medical" | "poetry";
-    sort: "relevance" | "newest";
+    category: category;
+    sort: sort;
     currentPage: number;
     totalPages: number;
-    isServerError: false;
+    isServerError: boolean;
     books: [] | BookType[];
-};
+}
+export enum booksActionsType {
+    FETCH_BOOKS = 'FETCH_BOOKS',
+    SET_ERROR = 'SET_ERROR',
+    SET_FILTER = 'SET_FILTER',
+    SET_SORT = 'SET_SORT',
+    SET_CATEGORY = 'SET_CATEGORY',
+    SET_CURRENT_PAGE = 'SET_CURRENT_PAGE',
+    SET_TOTAL_PAGE = 'SET_TOTAL_PAGE'
+}
 
+interface FetchBooksAction{
+    type: booksActionsType.FETCH_BOOKS;
+    payload: BookType[];
+}
+interface SetErrorAction{
+    type: booksActionsType.SET_ERROR;
+    payload: boolean;
+}
+interface SetFilterAction{
+    type: booksActionsType.SET_FILTER;
+    payload: string;
+}
+interface SetSortAction{
+    type: booksActionsType.SET_SORT;
+    payload: sort;
+}
+interface SetCategoryAction{
+    type: booksActionsType.SET_CATEGORY;
+    payload: category;
+}
+interface SetTotalPageAction{
+    type: booksActionsType.SET_TOTAL_PAGE;
+    payload: number;
+}
+interface SetCurrentPageAction{
+    type: booksActionsType.SET_CURRENT_PAGE;
+    payload: number;
+}
 
+export type bookActions = FetchBooksAction | SetErrorAction | SetFilterAction | SetSortAction | SetCategoryAction  | SetTotalPageAction | SetCurrentPageAction;
--- src/store/redux/books-reducer.ts
+++ src/store/redux/books-reducer.ts
@@ -1,4 +1,4 @@
-import {BookStateType} from "./books-reducer-types";
+import {BookStateType, booksActionsType, bookActions} from './books-reducer-types';
 
 export const initialBooksState: BookStateType = {
     filter: '',
@@ -10,8 +10,22 @@ export const initialBooksState: BookStateType = {
     books: []
 };
 
-const booksReducer = (state = initialBooksState, action: any): BookStateType => {
+const booksReducer = (state = initialBooksState, action: bookActions): BookStateType => {
     switch (action.type) {
+        case booksActionsType.FETCH_BOOKS :
+            return {...state, books: action.payload}
+        case booksActionsType.SET_ERROR :
+            return {...state, isServerError: action.payload}
+        case booksActionsType.SET_FILTER:
+            return {...state, filter: action.payload}
+        case booksActionsType.SET_SORT:
+            return {...state, sort: action.payload}
+        case booksActionsType.SET_CATEGORY:
+            return {...state, category: action.payload}
+        case booksActionsType.SET_TOTAL_PAGE:
+            return {...state, totalPages: action.payload}
+        case booksActionsType.SET_CURRENT_PAGE:
+            return {...state, currentPage: action.payload}
         default:
             return state
     }

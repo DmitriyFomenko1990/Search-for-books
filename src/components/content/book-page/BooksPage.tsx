8166f3877fe0947226c98c6714d0a69dab170aed
Dmitriy <fomenkodmitriy1990@gmail.com>
2021-08-19 01:51:32 +0300

    add dangerouslySetInnerHTML

--- src/components/content/book-page/BooksPage.tsx
+++ src/components/content/book-page/BooksPage.tsx
@@ -1,8 +1,5 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import style from './books-page.module.scss';
-import {useDispatch} from 'react-redux';
-import {fetchBook} from '../../../store/redux/action-creators';
-import { useRouteMatch } from 'react-router-dom';
 import {BookStateType} from '../../../store/redux/books-reducer-types';
 import {useTypedSelector} from '../../../store/redux/combine-reducers';
 
@@ -12,26 +9,30 @@ interface BooksPageType {
 }
 
 const BooksPage: React.FC<BooksPageType> = ({isFetch,setIsFetch}) => {
-
     const booksState: BookStateType = useTypedSelector(state => state.bookReducer);
     const categories = booksState.book.volumeInfo?.categories?.join(' / ');
     const  title = booksState.book.volumeInfo?.title;
     const  authors = booksState.book.volumeInfo?.authors?.join(', ');
-    const  description = booksState.book.volumeInfo?.description;
-
-
+    let  description: string = '';
+    if (booksState.book.volumeInfo?.description){
+        description = booksState.book.volumeInfo?.description;
+    }
+    function createDescriptionElement() {
+        return {
+            __html: description };
+    };
 
     return (
         <div className={style.wrapper}>
             <div className={style.coverWrapper}>
-                <img className={style.bookImage} src={booksState.book.volumeInfo?.imageLinks.thumbnail} />
+                <img className={style.bookImage} src={booksState.book.volumeInfo?.imageLinks?.thumbnail} />
             </div>
             <div className={style.aboutWrapper}>
                 <p className={style.categories}>{categories}</p>
                 <h3 className={style.title}>{title}</h3>
                 <p className={style.authors}>{authors}</p>
                 <div className={style.descWrapper}>
-                    <p className={style.description}>{description}</p>
+                    <div className={style.description} dangerouslySetInnerHTML={createDescriptionElement()}></div>
                 </div>
             </div>
         </div>

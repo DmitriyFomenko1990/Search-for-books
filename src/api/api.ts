f52b4ec08c35bf03f31d254f855423a4c2fb14c0
Dmitriy <fomenkodmitriy1990@gmail.com>
2021-08-18 13:16:42 +0300

    refactor api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: ` https://www.googleapis.com/books/v1/volumes?`,
+    baseURL: ` https://www.googleapis.com/books/v1/`,
 });
 
 export const booksAPI = {
     getBooks(filter:string, categories: string, start:number, sort:string) {
+        debugger
         return axiosInstance
-            .get(`q=${filter}+subject:${categories}&startIndex=${start}&maxResults=30&orderBy=${sort}&key=${process.env.REACT_APP_API_KEY}`)
-            .then(response =>  response)
+            .get(`volumes?q=${filter}+subject:${categories}&startIndex=${start}&maxResults=30&orderBy=${sort}&key=${process.env.REACT_APP_API_KEY}`)
+            .then(response =>  response.data)
     },
 };

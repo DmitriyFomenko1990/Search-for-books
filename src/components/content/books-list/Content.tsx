a7692057b41de630ac2942c3742ff86ac8ac7de6
Dmitriy <fomenkodmitriy1990@gmail.com>
2021-08-18 17:06:14 +0300

    add book list preloader

--- src/components/content/books-list/Content.tsx
+++ src/components/content/books-list/Content.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import style from './content.module.scss';
 import BooksList from '../BooksList';
-
+import {BookStateType} from "../../../store/redux/books-reducer-types";
+import {useTypedSelector} from "../../../store/redux/combine-reducers";
+import Loader from "../../loader/Loader";
 
 const Content: React.FC = () => {
-
+    const booksState: BookStateType = useTypedSelector(state => state.bookReducer);
+    const books = booksState.books;
     return (
        <div className={style.wrapper}>
-            <BooksList />
+           {books.length
+               ?<BooksList />
+               :<Loader />
+           }
        </div>
     );
-};
+}
 
 export default Content;
